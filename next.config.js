4aade16d4186ee9795fd6c48f3657a255ce7918c
Alaguraj1 <alaguraj259@gmail.com>
2025-03-29 10:54:31 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -19,10 +19,22 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['file.prade.in','prade.blr1.cdn.digitaloceanspaces.com','prade.blr1.digitaloceanspaces.com'], // Add your image domain here
+    // Add allowed external domains for image optimization
+    domains: [
+      'file.prade.in',
+      'prade.blr1.cdn.digitaloceanspaces.com',
+      'prade.blr1.digitaloceanspaces.com',
+    ],
+    // Optional: You can also specify image sizes and device sizes for optimization.
+    // Next.js automatically optimizes the images as per the user device and screen resolution.
+    deviceSizes: [320, 420, 768, 1024, 1200], // Widths for responsive images
+    imageSizes: [16, 32, 48, 64, 128, 256], // Pixel densities for the images
+    // Optional: Minimum cache TTL for optimized images
+    minimumCacheTTL: 60, // Time to keep the image in cache (in seconds)
   },
 };
 
 module.exports = nextConfig;
+
 
 
